62e2ba7faa653b48c0c57226dded5ba7f01f0a94
김소이 <thdl9893@ajou.ac.kr>
2024-10-02 02:07:48 +0900

    feat(#13): gateway jwt 검증필터 구현

---
+++ gateway/src/main/java/com/ticketing/gateway/configuration/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ticketing.gateway.configuration;
+
+import com.ticketing.gateway.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+//                        .pathMatchers("/api/v1/users/**").hasRole("USER")
+                        .anyExchange().authenticated()
+                )
+                .securityContextRepository(jwtAuthenticationFilter);
+
+        return http.build();
+    }
+}
