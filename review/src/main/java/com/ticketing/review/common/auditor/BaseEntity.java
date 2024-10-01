6f35fc50e9bc71f3e80b3796553e580ab6369600
JianBBB <sbganne@naver.com>
2024-10-02 05:33:25 +0900

    chore(#14): Entity에서 BaseEntity 필드 접근을 위해 접근 제한자 private -> protected로 변경

--- review/src/main/java/com/ticketing/review/common/auditor/BaseEntity.java
+++ review/src/main/java/com/ticketing/review/common/auditor/BaseEntity.java
@@ -18,28 +18,28 @@ public class BaseEntity {
 
   @CreatedDate
   @Column(name = "created_at", updatable = false)
-  private LocalDateTime createdAt;
+  protected LocalDateTime createdAt;
 
   @CreatedBy
   @Column(name = "created_by", updatable = false)
-  private Long createdBy;
+  protected Long createdBy;
 
   @LastModifiedDate
   @Column(name = "updated_at")
-  private LocalDateTime updatedAt;
+  protected LocalDateTime updatedAt;
 
   @LastModifiedBy
   @Column(name = "updated_by")
-  private Long updatedBy;
+  protected Long updatedBy;
 
   @Column(name = "deleted_at")
-  private LocalDateTime deletedAt;
+  protected LocalDateTime deletedAt;
 
   @Column(name = "deleted_by")
-  private Long deletedBy;
+  protected Long deletedBy;
 
   @Column(name = "is_deleted", nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-  private Boolean isDeleted = false;  // JPA 필드 초기화와 DB 기본값 설정을 함께 사용
+  protected Boolean isDeleted = false;  // JPA 필드 초기화와 DB 기본값 설정을 함께 사용
 
   protected void setDeleted(Long userId) {
     this.deletedAt = LocalDateTime.now();
