232c7450d1cdbcdbba58f6e59603cb11a528d43b
JianBBB <sbganne@naver.com>
2024-10-02 18:38:10 +0900

    feat(#14): 예외 처리 파일들 생성

--- review/src/main/java/com/ticketing/review/common/exception/GlobalExceptionHandelr.java
+++
@@ -1,5 +0,0 @@
-package com.ticketing.review.common.exception;
-
-public class GlobalExceptionHandelr {
-
-}
--- review/src/main/java/com/ticketing/review/common/exception/ReviewException.java
+++ review/src/main/java/com/ticketing/review/common/exception/ReviewException.java
@@ -1,16 +1,12 @@
 package com.ticketing.review.common.exception;
 
 import com.ticketing.review.common.response.ErrorCode;
-import lombok.Getter;
 
-@Getter
-public class ReviewException extends RuntimeException {
 
-  private final String message;
-  private final Integer statusCode;
+public class ReviewException extends BusinessException {
+
 
   public ReviewException(ErrorCode errorCode) {
-    this.message = errorCode.getMessage();
-    this.statusCode = errorCode.getHttpStatus().value();
+    super(errorCode);
   }
 }
