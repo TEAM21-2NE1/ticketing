d276fe244049a596f20d14d7c4a3807e66734b40
JianBBB <sbganne@naver.com>
2024-10-06 01:07:29 +0900

    feat(#27): 인증 인가 정보를 전역적으로 사용하기 위한 Util 추가

---
+++ review/src/main/java/com/ticketing/review/common/utils/SecurityUtils.java
@@ -0,0 +1,45 @@
+package com.ticketing.review.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Slf4j
+public class SecurityUtils {
+
+  public static Long getUserId() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    Long userId = null;
+    if (authentication != null) {
+      userId = Long.parseLong(authentication.getDetails().toString());
+      log.info("userId = {}", userId);
+    }
+
+    return userId;
+  }
+
+
+  public static String getUserRole() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    String userRole = null;
+    if (authentication != null) {
+      userRole = authentication.getAuthorities().toString();
+      log.info("userRole = {}", userRole);
+    }
+
+    return userRole;
+  }
+
+  public static String getUserEmail() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    String userEmail = null;
+    if (authentication != null) {
+      userEmail = authentication.getPrincipal().toString();
+    }
+
+    return userEmail;
+  }
+}
