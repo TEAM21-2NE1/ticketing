6a775446fe7eaffcaba31d20b0d97a892164739f
JianBBB <sbganne@naver.com>
2024-10-06 01:07:03 +0900

    feat(#27): Spring Security를 이용한 Header 인증 인가 정보 파싱 기능 구현

---
+++ review/src/main/java/com/ticketing/review/common/configuration/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.ticketing.review.common.configuration;
+
+import com.ticketing.review.common.filter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final JwtAuthorizationFilter jwtAuthenticationFilter;
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http,
+      AuthenticationManager authenticationManager) throws Exception {
+
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(session -> session.sessionCreationPolicy(
+            SessionCreationPolicy.STATELESS))  // 세션 사용하지 않음
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api/v1/orders/test-user").hasRole("USER")
+            .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
+        )
+        .addFilterBefore(jwtAuthenticationFilter,
+            UsernamePasswordAuthenticationFilter.class);  // JWT 인가 필터 추가
+
+    return http.build();
+  }
+}
