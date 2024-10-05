3c7006b828ee655d55d0952faa6fd15c5876149b
JianBBB <sbganne@naver.com>
2024-10-06 01:20:01 +0900

    rename(#27): SecurityUtils 파일 위치 이동

--- review/src/main/java/com/ticketing/review/infrastructure/utils/SecurityUtils.java
+++ review/src/main/java/com/ticketing/review/infrastructure/utils/SecurityUtils.java
@@ -1,4 +1,4 @@
-package com.ticketing.review.common.utils;
+package com.ticketing.review.infrastructure.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
