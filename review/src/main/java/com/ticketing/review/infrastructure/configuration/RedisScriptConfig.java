c9abc7c3f8f6bf594acefaea94c6319214fa2a4c
JianBBB <sbganne@naver.com>
2024-10-06 19:35:12 +0900

    feat(#30): Redis 및 Redis Script 설정 파일 추가

---
+++ review/src/main/java/com/ticketing/review/infrastructure/configuration/RedisScriptConfig.java
@@ -0,0 +1,26 @@
+package com.ticketing.review.infrastructure.configuration;
+
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.redis.core.script.RedisScript;
+
+@Configuration
+public class RedisScriptConfig {
+
+  @Bean
+  public RedisScript<Boolean> saveAvgRatingScript() throws IOException {
+    Resource scriptSource = new ClassPathResource("scripts/saveAvgRating.lua");
+    return RedisScript.of(scriptSource, Boolean.class);
+  }
+
+  @Bean
+  public RedisScript<String> getAvgRatingScript() throws IOException {
+    Resource scriptSource = new ClassPathResource("scripts/getAvgRating.lua");
+    return RedisScript.of(scriptSource, String.class);
+  }
+
+
+}
