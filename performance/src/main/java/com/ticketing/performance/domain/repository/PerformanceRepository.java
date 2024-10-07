c17497819c181cdff3012461c786095980e39814
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-10-08 01:12:37 +0900

    feat(#44): Performance 당일 티켓팅 오픈 공연 조회 메서드 추가

--- performance/src/main/java/com/ticketing/performance/domain/repository/PerformanceRepository.java
+++ performance/src/main/java/com/ticketing/performance/domain/repository/PerformanceRepository.java
@@ -1,15 +1,14 @@
 package com.ticketing.performance.domain.repository;
 
 import com.ticketing.performance.domain.model.Performance;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface PerformanceRepository extends JpaRepository<Performance, UUID>, CustomPerformanceRepository {
 
-    Page<Performance> findAllByOpenDateBefore(LocalDate date, Pageable pageable);
 
+    List<Performance> findAllByTicketOpenTimeBetween(LocalDateTime starOfDay, LocalDateTime endOfDay);
 }
