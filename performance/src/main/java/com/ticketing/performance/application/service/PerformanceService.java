678047b1f41d9177547db0b3723277accdaa6434
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-10-07 22:07:43 +0900

    feat(#42): performance 목록 조회  querydsl 이용한 custom repo 추가 및 service 메서드 변경

--- performance/src/main/java/com/ticketing/performance/application/service/PerformanceService.java
+++ performance/src/main/java/com/ticketing/performance/application/service/PerformanceService.java
@@ -13,16 +13,15 @@ import com.ticketing.performance.common.util.SecurityUtil;
 import com.ticketing.performance.domain.model.Performance;
 import com.ticketing.performance.domain.model.SeatStatus;
 import com.ticketing.performance.domain.repository.PerformanceRepository;
+import com.ticketing.performance.presentation.dto.PerformanceSearchRequestDto;
 import com.ticketing.performance.presentation.dto.performance.CreatePrfRequestDto;
 import com.ticketing.performance.presentation.dto.performance.UpdatePrfRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -49,8 +48,10 @@ public class PerformanceService {
     }
 
 
-    public Page<PrfListResponseDto> getPerformances(Pageable pageable) {
-        return performanceRepository.findAll(pageable).map(PrfListResponseDto::of);
+    public Page<PrfListResponseDto> getPerformances(PerformanceSearchRequestDto requestDto) {
+        return performanceRepository
+                .findAllByKeyword(requestDto.getKeyword(), requestDto.toPageable())
+                .map(PrfListResponseDto::of);
     }
 
     public PrfInfoResponseDto getPerformance(UUID performanceId) {
