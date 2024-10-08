ec3e67492101c9637f1594f10baf711c8e51e52d
JianBBB <sbganne@naver.com>
2024-10-08 23:18:21 +0900

    refactor(#46): JPARepository 상속 받던 ReviewRepositoryImpl을 Querydsl 인터페이스를 구현한 구현체로 변경

--- review/src/main/java/com/ticketing/review/infrastructure/repository/ReviewRepositoryImpl.java
+++ review/src/main/java/com/ticketing/review/infrastructure/repository/ReviewRepositoryImpl.java
@@ -1,26 +1,65 @@
 package com.ticketing.review.infrastructure.repository;
 
+import static com.ticketing.review.domain.model.QReview.review;
+import static org.springframework.util.StringUtils.hasText;
+
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ticketing.review.domain.model.Review;
-import com.ticketing.review.domain.repository.ReviewRepository;
-import java.util.Optional;
+import com.ticketing.review.domain.repository.ReviewRepositoryCustom;
+import java.util.List;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
 
-public interface ReviewRepositoryImpl extends JpaRepository<Review, UUID>, ReviewRepository {
+@Repository
+@RequiredArgsConstructor
+public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
 
-  boolean existsByUserIdAndPerformanceId(long userId, UUID performanceId);
-
-  @EntityGraph(attributePaths = {"reviewLikes"})
-  Optional<Review> findWithLikesById(UUID reviewId);
+  private final JPAQueryFactory queryFactory;
 
 
-  Page<Review> findByTitleContainingAndContentContaining(String title, String content,
-      Pageable pageable);
+  @Override
+  public Page<Review> findAllReviewByPageAndSortAndSearch(UUID performanceId, Pageable pageable,
+      String title, String content) {
 
-  Page<Review> findByTitleContaining(String title, Pageable pageable);
+    JPAQuery<Review> query = queryFactory
+        .selectFrom(review)
+        .where(titleContain(title), contentContain(content))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize());
 
-  Page<Review> findByContentContaining(String content, Pageable pageable);
+    for (Sort.Order order : pageable.getSort()) {
+      PathBuilder<Review> path = new PathBuilder<>(Review.class, "review");
+      query.orderBy(new OrderSpecifier(
+          com.querydsl.core.types.Order.valueOf(order.getDirection().name()),
+          path.get(order.getProperty())
+      ));
+    }
+    List<Review> list = query.fetch();
+
+    JPAQuery<Long> countQuery = queryFactory
+        .select(review.count())
+        .from(review)
+        .where(titleContain(title), contentContain(content));
+
+    return PageableExecutionUtils.getPage(list, pageable, countQuery::fetchOne);
+  }
+
+  private BooleanExpression titleContain(String title) {
+    return !hasText(title) ? null : review.title.contains(title);
+  }
+
+  private BooleanExpression contentContain(String content) {
+    return !hasText(content) ? null : review.content.contains(content);
+  }
+
+
 }
