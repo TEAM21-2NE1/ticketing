40631796fbc068e36dcd38b6354967131d7a2a37
de123456sdf <cdaeun95@gmail.com>
2024-10-11 19:27:16 +0900

    feat: global 설정

---
+++ payment/src/main/java/com/ticketing/payment/common/auditor/BaseTimeEntity.java
@@ -0,0 +1,50 @@
+package com.ticketing.payment.common.auditor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private Long deletedBy;
+
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean isDeleted = false;  // JPA 필드 초기화와 DB 기본값 설정을 함께 사용
+
+    protected void setDeleted(Long userId) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = userId;
+        this.isDeleted = true;
+    }
+}
---
+++ payment/src/main/java/com/ticketing/payment/common/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.ticketing.payment.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브(물류센터)를 찾을 수 없습니다."),
+    USER_INVALID_INPUT(HttpStatus.BAD_REQUEST, "유효하지 않은 입력값입니다."),
+
+    ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 값입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
