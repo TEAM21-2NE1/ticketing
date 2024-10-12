543d4021692ce6fcc31220de3482935e211850d0
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-10-12 16:48:53 +0900

    feat(#74): SeatException 예외 처리시 data가 있을때 포함해서 응답할수 있게 handler 추가 및 SeatException field 추가

--- performance/src/main/java/com/ticketing/performance/common/exception/GlobalExceptionHandler.java
+++ performance/src/main/java/com/ticketing/performance/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.ticketing.performance.common.exception;
 
+import com.ticketing.performance.application.dto.seat.SeatInfoResponseDto;
 import com.ticketing.performance.common.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -18,6 +21,15 @@ public class GlobalExceptionHandler {
                 .status(e.getErrorCode().getHttpStatus())
                 .body(CommonResponse.error(e.getMessage()));
     }
+
+    @ExceptionHandler(SeatException.class)
+    public ResponseEntity<CommonResponse<List<SeatInfoResponseDto>>> seatExceptionHandler(SeatException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(CommonResponse.errorWithData(e.getMessage(),e.getSeats()));
+    }
+
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
--- performance/src/main/java/com/ticketing/performance/common/exception/SeatException.java
+++ performance/src/main/java/com/ticketing/performance/common/exception/SeatException.java
@@ -1,11 +1,22 @@
 package com.ticketing.performance.common.exception;
 
+import com.ticketing.performance.application.dto.seat.SeatInfoResponseDto;
 import com.ticketing.performance.common.response.ErrorCode;
+import lombok.Getter;
 
-public class SeatException extends BusinessException{
+import java.util.List;
 
+@Getter
+public class SeatException extends BusinessException {
+
+    private List<SeatInfoResponseDto> seats;
 
     public SeatException(ErrorCode errorCode) {
         super(errorCode);
+    }
+
+    public SeatException(ErrorCode errorCode, List<SeatInfoResponseDto> seats) {
+        super(errorCode);
+        this.seats = seats;
     }
 }
