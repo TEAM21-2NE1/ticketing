3b9f776c4cffa713ac9c8271dfd651af80923adc
JianBBB <sbganne@naver.com>
2024-10-13 21:34:51 +0900

    feat(#73): reviewRepository에 공연 취소 시 리뷰 및 좋아요 삭제에 대한 query 추가

--- review/src/main/java/com/ticketing/review/domain/repository/ReviewRepository.java
+++ review/src/main/java/com/ticketing/review/domain/repository/ReviewRepository.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,4 +18,17 @@ public interface ReviewRepository extends JpaRepository<Review, UUID>, ReviewRep
 
   @EntityGraph(attributePaths = {"reviewLikes"})
   Optional<Review> findWithLikesById(UUID reviewId);
+
+  @Modifying(clearAutomatically = true)
+  @Query(value =
+      "UPDATE p_review_like SET is_deleted = true, deleted_at = CURRENT_TIMESTAMP, deleted_by = :userId "
+          +
+          "WHERE review_id IN (SELECT id FROM p_review WHERE performance_id = :performanceId); " +
+          "UPDATE p_review SET is_deleted = true, deleted_at = CURRENT_TIMESTAMP, deleted_by = :userId "
+          +
+          "WHERE performance_id = :performanceId", nativeQuery = true)
+  void deleteByPerformanceId(@Param("performanceId") UUID performanceId,
+      @Param("userId") Long userId);
+
+
 }
