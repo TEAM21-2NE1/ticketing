0e4038dfae1a91231492a5a680d3f7613a6e35f1
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-10-13 16:29:52 +0900

    fix(#78): getPerformance() openTime 이전 performance 조회시 예외 처리

--- performance/src/main/java/com/ticketing/performance/application/service/PerformanceService.java
+++ performance/src/main/java/com/ticketing/performance/application/service/PerformanceService.java
@@ -21,6 +21,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -56,6 +57,10 @@ public class PerformanceService {
     public PrfInfoResponseDto getPerformance(UUID performanceId) {
         Performance performance = performanceRepository.findById(performanceId)
                 .orElseThrow(() -> new PerformanceException(ErrorCode.PERFORMANCE_NOT_FOUND));
+
+        if (performance.getOpenDate().isAfter(LocalDate.now())) {
+            throw new PerformanceException(ErrorCode.PERFORMANCE_NOT_FOUND);
+        }
 
         HallInfoResponseDto hall = hallService.getHall(performance.getHallId());
 
