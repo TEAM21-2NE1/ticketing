b7bc001cf3889fe71109b072f4dacbd5b7537715
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-10-16 21:20:54 +0900

    feat(#123): EventApplicationService 추가

---
+++ performance/src/main/java/com/ticketing/performance/application/service/EventApplicationService.java
@@ -0,0 +1,19 @@
+package com.ticketing.performance.application.service;
+
+import com.ticketing.performance.application.event.EventSerializer;
+import com.ticketing.performance.application.event.PerformanceTopic;
+import com.ticketing.performance.application.event.PrfDeletedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EventApplicationService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishPerformanceDeletedEvent(PrfDeletedEvent event) {
+        kafkaTemplate.send(PerformanceTopic.DELETED.getTopic(), EventSerializer.serialize(event));
+    }
+}
