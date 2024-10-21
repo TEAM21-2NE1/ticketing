569d57c9c3a9618ea225ced528164ed2202a208e
JianBBB <sbganne@naver.com>
2024-10-21 12:53:59 +0900

    refactor(#134): 스케줄링 과정에서 리뷰 조회 캐시 삭제 기능 추가

--- review/src/main/java/com/ticketing/review/application/scheduler/AvgRatingScheduler.java
+++ review/src/main/java/com/ticketing/review/application/scheduler/AvgRatingScheduler.java
@@ -3,6 +3,8 @@ package com.ticketing.review.application.scheduler;
 import com.ticketing.review.domain.repository.RedisRatingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,11 @@ public class AvgRatingScheduler {
   private final RedisRatingRepository redisRatingRepository;
 
   @Scheduled(fixedRate = 3600000, zone = "Asia/Seoul")
+  @Caching(evict = {
+      @CacheEvict(cacheNames = "reviewCache", allEntries = true, cacheManager = "reviewCacheManager"),
+      @CacheEvict(cacheNames = {
+          "reviewSearchCache"}, allEntries = true, cacheManager = "reviewCacheManager")
+  })
   public void run() {
     redisRatingRepository.setAvgRatingBulk();
 
