807fc7eb38c7363d2af9032319d859f28b640ab5
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-10-25 00:44:13 +0900

    refactor(#149): 대기 번호 내뒤에 몇명 표시

--- order/src/main/java/com/ticketing/order/domain/model/WaitingTicket.java
+++ order/src/main/java/com/ticketing/order/domain/model/WaitingTicket.java
@@ -1,12 +1,19 @@
 package com.ticketing.order.domain.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(staticName = "of")
 @Getter
+@Builder
 public class WaitingTicket {
 
-    private final Long order; // 대기 순번
+    private final Long order;// 대기 순번
+    private final Long behindCount;
+
+    public static WaitingTicket of(Long order) {
+        return WaitingTicket.builder().order(order).build();
+    }
 
 }
